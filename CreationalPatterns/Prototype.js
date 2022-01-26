86d3703ea23c851ddd6a16f58b2784ae46b5caaf
Nushtaev Efim <efnushtaev@gmail.com>
2022-01-26 16:01:05 +0300

    protottype info improved

--- CreationalPatterns/Prototype.js
+++ CreationalPatterns/Prototype.js
@@ -1,12 +1,18 @@
 //
-// It is using for creating clone of objects contains prototype
+// ** It is using for creating clone of objects contains prototype
+//
+// ** Motivation:
+// Sometimes you need to make a copy of created object. For that ypu need to create a new object and then add a lot of fields from orig 
+// object and it can be a hard because of count of fileds. But we can make a special method for cloning a whole object.
+// 
+// ** When to use:
+// 1. You shouldn't depend on classes of the copied object
+// 2. The copied object has a lot of filled fields
 //
 
 Object.create();
 
-//
-// Realization by not main method
-//
+/* Realization by not JS main method */
 
 function PrototypeStock(proto) {
   this.proto = proto;
@@ -35,4 +41,4 @@ const prototypeStock = new PrototypeStock(newYorkStock);
 const stock = prototypeStock.clone(newYorkStock);
 
 console.log(stock.__proto__ === Stock.prototype)
-stock.info();
+stock.info();   
